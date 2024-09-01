36dff9fd2343fe78316f9a1286795b2b99fd0c46
Javier Bullrich <javier@bullrich.dev>
2024-09-01 19:52:19 +0200

    fixed empty private key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,13 +6,16 @@ import { envsafe, makeValidator, num, str } from "envsafe";
 
 const env = envsafe({
   ALCHEMY_API_KEY: str({
-    default: "ALCHEMY_API_KEY",
+    default: "",
+    allowEmpty: true,
   }),
   SEPOLIA_PRIVATE_KEY: str({
-    default: "SEPOLIA_PRIVATE_KEY",
+    default: "",
+    allowEmpty: true,
   }),
   ETHERSCAN_API_KEY: str({
-    default: "ETHERSCAN_API_KEY",
+    default: "",
+    allowEmpty: true,
   }),
 });
 
@@ -21,12 +24,14 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: env.ETHERSCAN_API_KEY,
   },
-  networks: {
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${env.ALCHEMY_API_KEY}`,
-      accounts: [env.SEPOLIA_PRIVATE_KEY],
-    },
-  },
+  networks: env.SEPOLIA_PRIVATE_KEY
+    ? {
+        sepolia: {
+          url: `https://eth-sepolia.g.alchemy.com/v2/${env.ALCHEMY_API_KEY}`,
+          accounts: [env.SEPOLIA_PRIVATE_KEY],
+        },
+      }
+    : {},
 };
 
 export default config;

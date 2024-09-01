a9544fd773b15050fba17ec3f8ddef5aa9c236c6
Javier Bullrich <javier@bullrich.dev>
2024-09-01 19:49:38 +0200

    tidiy up code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,20 +2,19 @@ import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomiclabs/hardhat-solhint";
 import "@nomicfoundation/hardhat-toolbox";
 
-import { envsafe, makeValidator, num, str } from 'envsafe';
+import { envsafe, makeValidator, num, str } from "envsafe";
 
 const env = envsafe({
   ALCHEMY_API_KEY: str({
-    default: "ALCHEMY_API_KEY"
+    default: "ALCHEMY_API_KEY",
   }),
   SEPOLIA_PRIVATE_KEY: str({
-    default: "SEPOLIA_PRIVATE_KEY"
+    default: "SEPOLIA_PRIVATE_KEY",
   }),
   ETHERSCAN_API_KEY: str({
-    default: "ETHERSCAN_API_KEY"
-  })
+    default: "ETHERSCAN_API_KEY",
+  }),
 });
-
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
@@ -27,7 +26,7 @@ const config: HardhatUserConfig = {
       url: `https://eth-sepolia.g.alchemy.com/v2/${env.ALCHEMY_API_KEY}`,
       accounts: [env.SEPOLIA_PRIVATE_KEY],
     },
-  }
+  },
 };
 
 export default config;
--- ignition/modules/Raffle.ts
+++ ignition/modules/Raffle.ts
@@ -1,24 +1,28 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-import { envsafe, makeValidator, num } from 'envsafe';
+import { envsafe, makeValidator, num } from "envsafe";
 
 import hre from "hardhat";
 
-
 export default buildModule("Deployment", (m) => {
-    const address = makeValidator<string>(input => {
-        if (!hre.ethers.isAddress(input)) {
-            throw new Error(`${input} is not a valid EVM address`);
-        }
-        return input;
-    })
-    const env = envsafe({
-        DONATION: address(),
-        PRICE: num(),
-        DURATION: num(),
-        TOKEN: address()
-    })
+  const address = makeValidator<string>((input) => {
+    if (!hre.ethers.isAddress(input)) {
+      throw new Error(`${input} is not a valid EVM address`);
+    }
+    return input;
+  });
+  const env = envsafe({
+    DONATION: address(),
+    PRICE: num(),
+    DURATION: num(),
+    TOKEN: address(),
+  });
 
-    const raffle = m.contract("Raffle", [env.DONATION, env.PRICE, env.DURATION, env.TOKEN]);
+  const raffle = m.contract("Raffle", [
+    env.DONATION,
+    env.PRICE,
+    env.DURATION,
+    env.TOKEN,
+  ]);
 
-    return { raffle };
+  return { raffle };
 });

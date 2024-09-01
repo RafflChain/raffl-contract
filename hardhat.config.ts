a6a4c6ac6f4f17298b2dd354c3856bff4d3ad44c
Javier Bullrich <javier@bullrich.dev>
2024-09-01 19:54:58 +0200

    Added automatic deployment using `workflow_dispatch` job (#2)
    Created an [ignition
    module](https://hardhat.org/ignition/docs/guides/creating-modules) and
    added a `workflow_dispatch` job to deploy new contracts.
    
    It also verifies the contract in Etherscan.
    
    The workflow also adds a summary with the address of the deployed
    contract.
    
    ## Summary
    
    - **New Features**
    - Introduced a new workflow for deploying smart contracts to the Sepolia
    network, allowing manual input of deployment parameters.
    - Added a module for deploying the "Raffle" contract with environment
    variable validation for improved reliability.
    
    - **Improvements**
    - Enhanced configuration for network and API key management in the
    Hardhat environment, ensuring secure interactions with external
    services.
    - Updated contract design to refine address management without direct
    Ether handling.
    
    - **Dependencies**
    	- Added `envsafe` for improved environment variable management.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,37 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomiclabs/hardhat-solhint";
 import "@nomicfoundation/hardhat-toolbox";
 
+import { envsafe, makeValidator, num, str } from "envsafe";
+
+const env = envsafe({
+  ALCHEMY_API_KEY: str({
+    default: "",
+    allowEmpty: true,
+  }),
+  SEPOLIA_PRIVATE_KEY: str({
+    default: "",
+    allowEmpty: true,
+  }),
+  ETHERSCAN_API_KEY: str({
+    default: "",
+    allowEmpty: true,
+  }),
+});
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  etherscan: {
+    apiKey: env.ETHERSCAN_API_KEY,
+  },
+  networks: env.SEPOLIA_PRIVATE_KEY
+    ? {
+        sepolia: {
+          url: `https://eth-sepolia.g.alchemy.com/v2/${env.ALCHEMY_API_KEY}`,
+          accounts: [env.SEPOLIA_PRIVATE_KEY],
+        },
+      }
+    : {},
 };
 
 export default config;

85558898598e4cd6073ac0e3886a7242ee3566e3
Javier Bullrich <javier@bullrich.dev>
2024-09-01 20:14:40 +0200

    added hardhat publish typechain library

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,10 @@
-import { HardhatUserConfig, vars } from "hardhat/config";
-import "@nomiclabs/hardhat-solhint";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-solhint";
+import 'hardhat-publish-typechain';
+import { HardhatUserConfig } from "hardhat/config";
 
-import { envsafe, makeValidator, num, str } from "envsafe";
+
+import { envsafe, str } from "envsafe";
 
 const env = envsafe({
   ALCHEMY_API_KEY: str({
@@ -17,6 +19,14 @@ const env = envsafe({
     default: "",
     allowEmpty: true,
   }),
+  NPM_TOKEN: str({
+    default: "npm-token",
+    allowEmpty: true,
+  }),
+  VERSION: str({
+    default: "0.0.0",
+    allowEmpty: true
+  })
 });
 
 const config: HardhatUserConfig = {
@@ -26,12 +36,21 @@ const config: HardhatUserConfig = {
   },
   networks: env.SEPOLIA_PRIVATE_KEY
     ? {
-        sepolia: {
-          url: `https://eth-sepolia.g.alchemy.com/v2/${env.ALCHEMY_API_KEY}`,
-          accounts: [env.SEPOLIA_PRIVATE_KEY],
-        },
-      }
+      sepolia: {
+        url: `https://eth-sepolia.g.alchemy.com/v2/${env.ALCHEMY_API_KEY}`,
+        accounts: [env.SEPOLIA_PRIVATE_KEY],
+      },
+    }
     : {},
+  publishTypechain: {
+    name: "raffle-contract",
+    repository: "https://github.com/Bullrich/raffle-contract",
+    version: env.VERSION,
+    includeDeployed: true,
+    iifeGlobalObjectName: "mock",
+    ignoreDeployedNetworks: ["localhost"],
+    authToken: env.NPM_TOKEN,
+  }
 };
 
 export default config;

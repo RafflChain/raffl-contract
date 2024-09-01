ec6d64abb3795537ebb11d0262db3f766334ece7
Javier Bullrich <javier@bullrich.dev>
2024-09-01 19:14:25 +0200

    added network information to config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomiclabs/hardhat-solhint";
 import "@nomicfoundation/hardhat-toolbox";
 
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  }
 };
 
 export default config;

ce1b7cc25ee9f7d8666dac4a5545be9bb122d678
Javier Bullrich <javier@bullrich.dev>
2024-09-04 14:47:11 +0200

    fixed failing test

--- test/Raffle.ts
+++ test/Raffle.ts
@@ -11,7 +11,7 @@ describe("Raffle", function () {
   const PRICE_100_TICKET_MULTIPLIER = 60n;
 
   async function deployRaffleFixture() {
-    const ticketPrice = 5n;
+    const ticketPrice = 2n;
     const unlockDays = 2;
 
     // Contracts are deployed using the first signer/account by default
@@ -19,14 +19,14 @@ describe("Raffle", function () {
     const [owner] = signers;
     const players = signers.slice(2);
 
-    const DemoToken = await hre.ethers.getContractFactory("DemoToken");
+    const DemoToken = await hre.ethers.getContractFactory("RaffleToken");
     const token = await DemoToken.deploy();
     const tokenAddress = await token.getAddress();
 
     const decimals: bigint = await token.decimals();
-    for (const player of players) {
+    for (let i = 0; i < 5; i++) {
       const transferAmount = ticketPrice * 10n ** decimals * 100n;
-      await token.connect(owner).transfer(player.address, transferAmount);
+      await token.connect(owner).transfer(players[i].address, transferAmount);
     }
 
     const Raffle = await hre.ethers.getContractFactory("Raffle");
@@ -387,7 +387,7 @@ describe("Raffle", function () {
           .approve(
             await raffle.getAddress(),
             ticketPrice *
-              (PRICE_100_TICKET_MULTIPLIER + PRICE_10_TICKET_MULTIPLIER + 1n),
+            (PRICE_100_TICKET_MULTIPLIER + PRICE_10_TICKET_MULTIPLIER + 1n),
           );
         // Buy 1 ticket and verify that the player has 1
         await raffle.connect(player).buySingleTicket();
@@ -446,11 +446,11 @@ describe("Raffle", function () {
         );
         expect(await raffle.pot()).to.equal(
           ticketPrice *
-            (PRICE_100_TICKET_MULTIPLIER + PRICE_10_TICKET_MULTIPLIER + 1n),
+          (PRICE_100_TICKET_MULTIPLIER + PRICE_10_TICKET_MULTIPLIER + 1n),
         );
         expect(await token.balanceOf(rAddress)).to.equal(
           ticketPrice *
-            (PRICE_100_TICKET_MULTIPLIER + PRICE_10_TICKET_MULTIPLIER + 1n),
+          (PRICE_100_TICKET_MULTIPLIER + PRICE_10_TICKET_MULTIPLIER + 1n),
         );
       });
     });
